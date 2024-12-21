2c485a20099fbba658c0a021ff6e4ad968ea208f
Nian0bussou <nianlegare@outlook.com>
2024-12-20 19:46:51 -0500

    set

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -3,9 +3,9 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -36,8 +36,8 @@ vim.opt.updatetime = 50
 -- vim.opt.colorcolumn = "80"
 
 
-vim.opt.list = true
-vim.opt.listchars:append("space:·")
+-- vim.opt.list = true
+-- vim.opt.listchars:append("space:·")
 
 
 
