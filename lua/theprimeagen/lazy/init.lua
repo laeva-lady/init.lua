1c83cdbd3d75e55a341d4c47f0d744e6508d48dd
Nian0bussou <nianlegare@outlook.com>
2024-12-30 18:42:41 -0500

    added black formatter for python"

--- lua/theprimeagen/lazy/init.lua
+++ lua/theprimeagen/lazy/init.lua
@@ -5,6 +5,13 @@ return {
         name = "plenary"
     },
 
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function ()
+            require"theprimeagen.configs.null"
+        end
+    },
+
     "eandrju/cellular-automaton.nvim",
 }
 
